3ba3bba9babc17a122a4ab5c1f95c833cbb5d556
yuwang <you@example.com>
2022-06-13 22:55:56 +0800

    project construction

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { memo } from "react" 
+
+import routes from "./router";
+import {  useRoutes } from "react-router-dom";
+
+import HYAppHeader from "components/app-header";
+import HYAppFooter from "components/app-footer";
+
+export default memo(function App() {
+  const element = useRoutes(routes)
+  return (
+    <div>
+      <HYAppHeader />
+        {element}
+      <HYAppFooter />
+    </div>    
+  );
+})
+
